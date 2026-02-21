21a580854eadd3004d094f5f5cf6bfae6da06e04
Sanad <Youssefsanadd@gmail.com>
2026-02-21 13:39:22 +0200

    Edit Sparse Table : Sparse_Table.cpp

--- Sparse_Table.cpp
+++ Sparse_Table.cpp
@@ -1,60 +1,93 @@
-/*
-      Sparse Table ( RMQ : Range Minimum Query ) --- Static
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 50005 ;
-const int Log = 18 ;
-vector<vector<int>> Ans( N, vector<int>( 18 ) ) ;
-// Ans [ i ][ j ] indicates the Answer from i to i + 2^j - 1
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 
-int operation( int x, int y )
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>,
+                         rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T>
+using ordered_multiset = tree<T, null_type, less_equal<T>,
+                              rb_tree_tag, tree_order_statistics_node_update>;
+
+#define min_nums(n) st.order_of_key(n)
+#define value(n) st.find_by_order(n)
+#define ll long long
+#define int long long
+#define ull unsigned long long
+#define input(v)        \
+      for (auto &x : v) \
+      cin >> x
+#define output(v)       \
+      for (auto &x : v) \
+            cout << x << " ";
+#define all(v) v.begin(), v.end()
+#define x first
+#define y second
+
+class SparseTable
 {
-      return min( x, y ) ;
-}
-void SparseTable( vector<int> &a ) // O ( n * log( n ) ) 
-{
-      int n = a.size() ;
+private:
+      const int N = 500005;
+      const int LOG = 22;
+      vector<vector<int>> Ans;
 
-      for ( int i = 0 ; i < n ; i++ )
-            Ans[i][0] = a[i] ;
-
-      for ( int j = 1 ; j < Log ; j++ )
+      int merge(int x, int y)
       {
-            for ( int i = 0 ; i + ( 1 << (j-1) ) < n ; i++ )
+            return min(x, y);
+      }
+
+public:
+      SparseTable()
+      {
+            Ans.assign(N, vector<int>(LOG, 0));
+      }
+
+      void build(vector<int> &nums)
+      {
+            int n = nums.size();
+
+            for (int i = 0; i < n; i++)
+                  Ans[i][0] = nums[i];
+
+            for (int j = 1; j < LOG; j++)
             {
-                  Ans[i][j] = operation( Ans[i][j-1], Ans[i+(1<<(j-1))][j-1] ) ;
+                  for (int i = 0; i + (1 << (j - 1)) < n; i++)
+                  {
+                        Ans[i][j] = merge(Ans[i][j - 1], Ans[i + (1 << (j - 1))][j - 1]);
+                  }
             }
       }
-}
 
-int Log_Query( int l, int r ) // O ( Log( n ) )
-{
-      int Len = r - l + 1 ;
-      int res = INT_MAX ;
-      for ( int j = Log-1 ; j >= 0 ; j-- )
+      int Log_query(int l, int r)
       {
-            if ( Len & ( 1 << j ) )
+            int len = r - l + 1;
+            int ans = LLONG_MAX;
+            for (int i = LOG - 1; i >= 0; i--)
             {
-                  res = operation( res, Ans[l][j] ) ;
+                  if (len & (1 << i))
+                  {
+                        ans = merge(ans, Ans[l][i]);
+                        l += (1 << i);
+                  }
             }
-            l += ( 1 << j ) ;
+            return ans;
       }
-      return res ;
-}
 
-int One_Query( int l, int r ) // O ( 1 )
-{
-      /*
-            Works only for idempotent associative operations,
-            where overlapping segments do not affect the result.
-            - Works on ( Minimum, Maximum, Gcd, And, Or, .... ).
-            - Doesn't work on ( Sum, XOR, Product ...... ).
-      */
-
-      int Len = r - l + 1 ;
-      int k = 31 - __builtin_clz( Len ); // K indicates the log of Len. You can also use a pre-calculation method for logs.
-      int res = operation( Ans[l][k], Ans[r-(1<<k)+1][k] ) ;
-      return res ;
-}
+      int One_query(int l, int r)
+      {
+            /*
+                  Works only for idempotent associative operations,
+                  where overlapping segments do not affect the result.
+                  - Works on ( Minimum, Maximum, Gcd, And, Or, .... ).
+                  - Doesn't work on ( Sum, XOR, Product ...... ).
+            */
+            int len = r - l + 1;
+            int k = 31 - __builtin_clz(len);
+            int ans = merge(Ans[l][k], Ans[r - (1 << k) + 1][k]);
+            return ans;
+      }
+};
