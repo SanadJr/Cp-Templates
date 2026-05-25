ea5c5709b85d8970826e7f73c0c50a83e4d48eae
youssef-sanad <youssefsanadd@gmail.com>
2026-05-25 18:46:04 +0300

    Add kuhn algorithm

---
+++ graphs/kuhn-algorithm.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// o( n * m )
+int n, m, k ;
+vector<int> adj[505] ;
+int match[505] ;
+bool used[505] = {} ;
+
+bool Try_Kuhn( int u )
+{
+      if ( used[u] )
+            return false ;
+
+      used[u] = true ;
+      for ( int v : adj[u] )
+      {
+            if ( match[v] == -1 || Try_Kuhn( match[v] ) )
+            {
+                  match[v] = u ;
+                  return true ;
+            }
+      }
+      return false ;
+}
+void solve()
+{
+      cin >> n >> m >> k ;
+      memset( match, -1, sizeof match ) ;
+      while ( k-- )
+      {
+            int u, v ;
+            cin >> u >> v ;
+            adj[u].push_back( v ) ;
+      }
+
+      int res = 0 ;
+      for ( int i = 1 ; i <= n ; i++ )
+      {
+            for ( int j = 0 ; j < 505 ; j++ )
+                  used[j] = false ;
+            if ( Try_Kuhn( i ) )
+                  res++ ;
+      }
+
+      cout << res ;
+
+}
